ac2446ac8701d0e1a08aa3f259085577f61f7018
Marcel <info@prodevmo.com>
2020-09-29 20:59:21 +0200

    .

--- src/src_buildroot/buildroot/package/atcrpc/atcrpc.mk
+++ src/src_buildroot/buildroot/package/atcrpc/atcrpc.mk
@@ -4,10 +4,37 @@
 #
 ################################################################################
 
-ATCRPC_VERSION = origin/rpclib
-ATCRPC_SITE = git@github.com:RBEGamer/AtomicChessOS.git
+ATCRPC_VERSION = origin/master
+ATCRPC_SITE = git@github.com:RBEGamer/rpclib.git
 ATCRPC_SITE_METHOD = git
 ATCRPC_LICENSE = MIT
 ATCRPC_INSTALL_TARGET = YES
 
-$(eval $(cmake-package))
+#
+define ATCRPC_BUILD_CMDS
+	@echo ATCRPC_BUILD!
+#	ls $(@D)
+#	@echo $(@D)
+	#$(MAKE) --version
+#	
+	@echo -----------------
+	cmake $(@D)
+	#$(@D)/build
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)
+	#$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/gpio
+	#$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/examples
+endef
+
+
+define ATCRPC_INSTALL_TARGET_CMDS
+	@echo ATCRPC_INSTALL!
+	#$(INSTALL) -D -m 0755 $(@D) $(TARGET_DIR)/usr
+	#$(INSTALL) -D -m 0755 $(@D)/gpio $(TARGET_DIR)/usr
+	#$(INSTALL) -D -m 0755 $(@D)/gpio $(TARGET_DIR)/examples
+endef
+
+$(eval $(generic-package))
+
+
+
+#$(eval $(cmake-package))
