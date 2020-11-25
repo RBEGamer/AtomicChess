a0ba98a3366388c7b63bb99329aa90c41d6c3c21
Marcel <info@prodevmo.com>
2020-11-25 21:29:20 +0100

    .

--- src/src_server/CI/start_stack.sh
+++ src/src_server/CI/start_stack.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 clear
-docker-compose down
+docker-compose down -v
 docker-compose build --no-cache
 
 docker-compose up -d AtomicChessMongoDatabase AtomicChessRedisDatabase AtomicChessMoveValidator redis-commander
 
-sleep 10s
+sleep 30s
 docker-compose up -d AtomicChessBackend
-sleep 10s
+sleep 30s
 docker-compose up -d AtomicChessAutoPlayer
