2ebff0141d5d39981a9bb6dcd94cc203095412b3
Marcel <info@prodevmo.com>
2021-03-14 16:50:39 +0100

    .

--- src/src_thesis/build_report.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "-- STARTING BUILDING REPORT DOCUMENT --"
-
-pandoc "report_document.md" -o "report_document.tex" --from markdown --template "template.tex" --listings
-
-pdflatex report_document.tex report_document.pdf
-
-exit 0
--- src/src_thesis/build_thesis.sh
+++ src/src_thesis/build_thesis.sh
@@ -2,7 +2,8 @@
 
 echo "-- STARTING BUILDING THESIS DOCUMENT --"
 
-pandoc "thesis_document.md" -o "thesis_document.tex" --from markdown --template "template.tex" --listings
+#pandoc "thesis_document.md" -o "thesis_document.tex" --from markdown --template "template.tex" --listings
+pandoc "thesis_document.md" -o "thesis_document.tex" --from markdown --listings
 
 pdflatex thesis_document.tex thesis_document.pdf
 
