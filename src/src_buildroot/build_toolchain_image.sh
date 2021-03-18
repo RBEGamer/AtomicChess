4fd2e39f57400029bbfaf1ebb888be44e0b749c9
Marcel <info@prodevmo.com>
2021-03-18 21:18:59 +0100

    skip image build if atcbuildroot:latest image exists

--- src/src_buildroot/build_toolchain_image.sh
+++ src/src_buildroot/build_toolchain_image.sh
@@ -2,7 +2,7 @@
 
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
-  exit
+  exit -1
 fi
 
 # COPY PACKAGE SOURCES
--- src/src_buildroot/buildroot/build_docker.sh
+++ src/src_buildroot/buildroot/build_docker.sh
@@ -4,7 +4,15 @@ bash ./restore_config.sh
 #rm -Rf ./output
 
 # BUILD CONTAINER
-docker build -t atcbuildroot .
+
+if [[ "$(docker images -q atcbuildroot:lates 2> /dev/null)" == "" ]]; then
+  # do something
+  echo "atcbuildroot:lates IMAGE EXISTS; NO BUILD REQUIRED"
+else
+ echo "atcbuildroot:lates IMAGE BUILD STARTED"
+ docker build -t atcbuildroot:latest .
+fi
+
 
 # COPY FILES OVER TO HOST USING SCIRPT INSIDE OF THE CONTAINER
 docker run -i --rm -v "$(pwd)":/var/buildroot atcbuildroot
