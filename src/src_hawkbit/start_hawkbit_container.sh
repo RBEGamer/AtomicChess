7c052d676cb89e91d4100501b0c0261bf68f5fca
Marcel <info@prodevmo.com>
2021-03-18 22:53:56 +0100

    added hawkbit config + docker compose

--- src/src_hawkbit/start_hawkbit_container.sh
+++ src/src_hawkbit/start_hawkbit_container.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cd ./jenkins_conf && docker container run --name jenkins-blueocean --restart always --env DOCKER_HOST=tcp://docker:2376 -v $(pwd):/var/jenkins_home -p 8080:8080 -p 50000:50000 jenkinsci/blueocean
+docker-compose up -d
