e4e8005dfbc4a94c74db363b31150caeca8411a6
Marcel <info@prodevmo.com>
2021-04-29 22:07:03 +0200

    .

---
+++ src/src_controller/controller/controller/VoiceRecognitionExtention.cpp
@@ -0,0 +1,5 @@
+//
+// Created by prodevmo on 29.04.21.
+//
+
+#include "VoiceRecognitionExtention.h"
---
+++ src/src_controller/controller/controller/VoiceRecognitionExtention.h
@@ -0,0 +1,14 @@
+//
+// Created by prodevmo on 29.04.21.
+//
+
+#ifndef CONTROLLER_VOICERECOGNITIONEXTENTION_H
+#define CONTROLLER_VOICERECOGNITIONEXTENTION_H
+
+
+class VoiceRecognitionExtention {
+
+};
+
+
+#endif //CONTROLLER_VOICERECOGNITIONEXTENTION_H
