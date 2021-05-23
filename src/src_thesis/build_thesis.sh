5ad93a4d72fbcf603c1356586c2ffcf7037703cb
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2021-05-23 02:31:46 +0200

    add pandoc chapter option

--- src/src_thesis/build_thesis.sh
+++ src/src_thesis/build_thesis.sh
@@ -37,7 +37,7 @@ pandoc --version
 
 
 
-pandoc ./thesis_document.md -o ./thesis_document.tex --from markdown --biblatex --template ./pandoc_template.tex --listings --lua-filter ./pandoc_filters/pandoc-gls.lua
+pandoc ./thesis_document.md -o ./thesis_document.tex --from markdown --biblatex --template ./pandoc_template.tex --listings --top-level-division=chapter --lua-filter ./pandoc_filters/pandoc-gls.lua
 # NOW THE HACKY PART WE WANT TO USE THE STANDART cite command instead the from pandoc used cite to we use sed to hard replace the stuff
 sed -i 's/\\autocite{/\\cite{/g' ./thesis_document.tex
 
