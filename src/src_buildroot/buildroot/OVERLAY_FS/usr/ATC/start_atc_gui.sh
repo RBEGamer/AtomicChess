3f52cafcd8739a6716e7032c4f750f2b029ef562
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2021-06-16 23:52:12 +0200

    Update start_atc_gui.sh

--- src/src_buildroot/buildroot/OVERLAY_FS/usr/ATC/start_atc_gui.sh
+++ src/src_buildroot/buildroot/OVERLAY_FS/usr/ATC/start_atc_gui.sh
@@ -4,7 +4,7 @@ while true; do
 	FILEA=/usr/ATC/ATC_LOCK
 	if test -f "$FILEA"; then
     		echo "STARTING ATC GUI"
-		/usr/ATC/atc_ui
+		/usr/ATC/atc_ui --platform directfb
 		sleep 10
 	else
 	    	exit 1
