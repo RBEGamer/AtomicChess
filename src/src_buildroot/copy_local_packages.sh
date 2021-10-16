ed4ed004188ef94a815faf0cc7f2e418297afcdf
Marcel Ochsendorf <marcel.ochsendorf@gmail.com>
2021-10-16 22:08:34 +0200

    Create copy_local_packages.sh

---
+++ src/src_buildroot/copy_local_packages.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd ../src_atctest && bash copy_to_buildroot_package.sh && cd ../src_buildroot
+cd ../src_atcqtui && bash copy_to_buildroot_package.sh && cd ../src_buildroot
+cd ../src_controller && bash copy_to_buildroot_package.sh && cd ../src_buildroot
